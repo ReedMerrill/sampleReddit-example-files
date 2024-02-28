c40403bc4265e72806494e22be4e4dc66a5d477a
Reed <reedjmerrill@gmail.com>
2024-02-28 16:46:40 -0700

    rename and put snowball sampling of users and collection of their comments in the same file

--- scripts/example-comment-sampling.py
+++ scripts/example-comment-sampling.py
@@ -10,8 +10,6 @@ Pre-requisites:
 """
 
 import json
-import time
-import datetime
 import sampleReddit as sr
 
 # =============================================================================
@@ -42,7 +40,7 @@ instance = sr.setup_access(
 # set subreddit sample parameters
 # =============================================================================
 # the names of subreddits to sample
-subreddits = ["politics", "Republican", "democrats"]
+subreddits_list = ["politics", "Republican", "democrats"]
 # How to filter posts within a subreddit. Can be "top", "new", or "hot".
 filter = "top"
 # How far back to go. Can be "all", "day", "hour", "month", "week", or "year" .
@@ -53,10 +51,10 @@ n_posts = 1
 # =============================================================================
 # Call the sampling function
 # =============================================================================
-# sample the subreddits
+# sample the subreddits you specified in subreddits_list
 sampling_frame, users_df = sr.sample_reddit(
     api_instance=instance,
-    seed_subreddits=subreddits,
+    seed_subreddits=subreddits_list,
     post_filter=filter,
     time_period=time_period,
     n_posts=n_posts,
@@ -64,10 +62,33 @@ sampling_frame, users_df = sr.sample_reddit(
 )
 
 # =============================================================================
-# Save the data
+# Save the sample frame data and list of users
 # =============================================================================
 # save the sampling frame
 with open(f"{OUTPUT_PATH}sampling_frame.json", "w") as f:
     json.dump(sampling_frame, f, indent=2)
 # save the users dataframe
 users_df.to_csv(f"{OUTPUT_PATH}users.csv", index=False)
+
+# =============================================================================
+# Collect Comments
+# =============================================================================
+"""This section takes the list of users that you just generated and collects
+comments for every user. The maximum number of comments that Reddit allows is
+1,000. It also saves the username of the user who made each comment, the IDs of
+the post and subreddit the comment was made on, the comment's timestamp, the
+parent comment, and the comment's upvotes.
+
+It saves this data to the location specified in COMMENTS_OUTPUT.
+"""
+
+USERS_LIST = f"{PROJECT_PATH}data/user.csv"
+COMMENTS_OUTPUT = f"{OUTPUT_PATH}data/comments.csv"
+
+sr.get_comments(
+    api_instance=instance,
+    input_path=USERS_LIST,
+    output_path=COMMENTS_OUTPUT,
+    log_path=LOG_FILE_PATH,
+    comment_limit=1000,
+)
